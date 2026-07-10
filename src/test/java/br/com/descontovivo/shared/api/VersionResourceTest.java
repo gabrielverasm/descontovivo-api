935f71cdb70fe99d82da97496c91057d1fe37b4f
Gabriel Veras <gabrielverasm@gmail.com>
2026-07-10 10:26:47 -0300

    Feat/promotions trust signals api (#44)
    * feat(promotions): add trust signals support
    
    * test(shared): update API version expectation

--- src/test/java/br/com/descontovivo/shared/api/VersionResourceTest.java
+++ src/test/java/br/com/descontovivo/shared/api/VersionResourceTest.java
@@ -16,7 +16,7 @@ class VersionResourceTest {
             .then()
             .statusCode(200)
             .body("name", is("descontovivo-api"))
-            .body("version", is("0.2.1"));
+            .body("version", is("0.3.0"));
     }
 
     @Test
